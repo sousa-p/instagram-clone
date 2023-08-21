c03939946641a3bde1a18bb8d0849ebf5a294117
sousa-p <p.soousa7@gmail.com>
2023-08-21 15:08:49 -0300

    Começando UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-4">
+                <img src="/images/foto-perfil.jpg" class="rounded-circle border" alt="Foto de perfil" srcset="">
+            </div>
+            <div class="col-5 pt-4">
+                <div>
+                    <h1>instagram</h1>
+                </div>
+                <div class="row pt-3 pb-3">
+                    <div class="col-4"><strong>7,477</strong> posts</div>
+                    <div class="col-4"><strong>652M</strong> followers</div>
+                    <div class="col-4"><strong>95</strong> following</div>
+                </div>
+                <div class="">
+                    <strong>Instagram</strong>
+                </div>
+                <div class="">
+                    <p>Discover what's next on Instagram 🔎✨</p>
+                </div>
+                <div class=""><a href="#">
+                    www.threads.net</a></div>
+            </div>
+        </div>
+    </div>
+@endsection
