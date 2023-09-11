0df5ff3d8628e499f47818b0a375e8e467cc53d3
sousa-p <p.soousa7@gmail.com>
2023-09-10 21:24:05 -0300

    Criando migration Post, e suas relações

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function profile() {
+    public function profile()
+    {
         return $this->hasOne(Profile::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
     }
 }
